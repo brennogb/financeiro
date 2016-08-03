c1c1d7363bd74215033eda655ab6d737b0be8d96
Brenno Gomes <brennogomes23@gmail.com>
2016-08-02 22:03:27 -0300

    criação da classe DAOFactory até a configuracao do xml do Hibernate

---
+++ src/main/java/br/com/javaparaweb/financeiro/util/HibernateUtil.java
@@ -0,0 +1,27 @@
+package br.com.javaparaweb.financeiro.util;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+	private static final SessionFactory sessionFactory = buildSessionFactory();
+	
+	private static SessionFactory buildSessionFactory() {
+		try {
+			Configuration cfg = new Configuration();
+			cfg.configure("hibernate.cfg.xml");
+			StandardServiceRegistryBuilder registradorServico = new StandardServiceRegistryBuilder();
+			registradorServico.applySettings(cfg.getProperties());
+			StandardServiceRegistry servico = registradorServico.build();
+			return cfg.buildSessionFactory(servico);
+		} catch (Throwable e) {
+			throw new ExceptionInInitializerError(e);
+		}
+	}
+	
+	public static SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+}
